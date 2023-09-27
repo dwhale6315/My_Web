8ffa0923d9b38c0f76c6925a08d03771c09aa40d
dwhale6315 <88151404+dwhale6315@users.noreply.github.com>
2023-09-27 21:00:23 +0900

    d
    d

---
+++ coding.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="ko-KR">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gojomog</title>
+    <style>
+        a{
+            color: #000;
+            text-decoration: none;
+        }
+        h1{
+            font-size: 45px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>다양한 프로그래밍 언어에 대해 알아봅시다.</h1>
+    <nav>
+        <ol>
+            <li><a href="https://dwhale6315.github.io/My_Web/html.html">HTML</a></li>
+        </ol>
+    </nav>
+    
+</body>
+</html>
